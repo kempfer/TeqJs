91913dc99224c514b0b9fbe5ed766d78dca72171
Maxim <zotov_mv@groupbwt.com>
2014-09-25 17:44:37 +0300

    fixed object triangle
    Signed-off-by: Maxim <zotov_mv@groupbwt.com>

--- src/canvas/TCanvasTriangle.js
+++ src/canvas/TCanvasTriangle.js
@@ -1,46 +1,40 @@
-t.Class.define('t.canvas.triangle', {
-	
-	Extend : t.canvas.object,
-	
-	_x : 0,
-	
-	_y : 0,
-	
-	/**
-	* Constructor
-	* @param {Object} [options] Options object
-	* @return {Object} thisArg
-	*/
-	init : function initTriangle (options) {
-		this.defaultOptions = t.canvas.triangle.options;
-		this.callParent(options);
-	},
-	_render : function (ctx) {	
-		this._counted();
-		var widthByTo = this.getWidth() / 2,
-			heightByTo = this.getHeight() / 2;
-	    ctx.beginPath();
-	    ctx.moveTo(-widthByTo , heightByTo);
-	    ctx.lineTo(0, - heightByTo);
-	    ctx.lineTo(widthByTo, heightByTo);
-	    ctx.closePath();
-		this._renderFill(ctx);
-		this._renderStroke(ctx);
-	},
-	_transform : function (ctx) {
-		ctx.translate(this.getX() , this.getY());
-		if(this.getAngle()) {
-			
-			ctx.rotate(t.canvas.degree(this.getAngle()));
-		}
-	},
-	_counted : function () {
-		this._x = (!this.getAngle()) ? this.getX() : - this.getWidth()/2;
-		this._y = (!this.getAngle()) ? this.getY() : - this.getHeight()/2;
-	}
-});
+(function (t) {
+	'use strict';
 
-t.canvas.triangle.options = t.combine (t.canvas.object.options, {
-	width: 50,
-	height: 50,
-});
+	t.Class.define('t.canvas.triangle', {
+		
+		Extend : t.canvas.object,
+		
+		/**
+		* Constructor
+		* @param {Object} [options] Options object
+		* @return {Object} thisArg
+		*/
+		init : function initTriangle (options) {
+			this.defaultOptions = t.canvas.triangle.options;
+			this.callParent(options);
+		},
+		_render : function (ctx) {	
+			this._counted();
+			var widthByTo = this.getWidth() / 2,
+				heightByTo = this.getHeight() / 2;
+			ctx.beginPath();
+			ctx.moveTo(this._x + widthByTo , this._y);
+			ctx.lineTo(this._x + this.getWidth(), this._y + this.getHeight());
+			ctx.lineTo(this._x, this._y + this.getHeight());
+			ctx.closePath();
+			this._renderFill(ctx);
+			this._renderStroke(ctx);
+		},
+		_counted : function () {
+			this._x = (!this.isTransform()) ? this.getX()/this.getScaleX() : - this.getWidth()/2;
+			this._y = (!this.isTransform()) ? this.getY()/this.getScaleY()  : - this.getHeight()/2;
+		}
+	});
+
+	t.canvas.triangle.options = t.combine (t.canvas.object.options, {
+		width: 50,
+		height: 50,
+	});
+
+})(window.t)
